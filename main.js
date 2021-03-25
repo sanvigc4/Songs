e144f777271d52a7234a2dbb9b039ccd0f959aa1
sanvigc4 <79517431+sanvigc4@users.noreply.github.com>
2021-03-25 12:07:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function playsong1(){
+    document.getElementById("Apsara Ali").play();
+}
+function pausesong1(){
+    document.getElementById("Apsara Ali").pause();
+}
+function playsong2(){
+    document.getElementById("Londoon Thumakada").play();
+}
+function pausesong2(){
+    document.getElementById("Londoon Thumakada").pause();
+}
+function playsong3(){
+    document.getElementById("Chhalka Chhalka ree").play();
+}
+function pausesong3(){
+    document.getElementById("Chhalka Chhalka ree").pause();
+}
+function playsong4(){
+    document.getElementById("Maahi Ve").play();
+}
+function pausesong4(){
+    document.getElementById("Maahi Ve").pause();
+}
+function playsong5(){
+    document.getElementById("Beilever").play();
+}
+function pausesong5(){
+    document.getElementById("Beilever").pause();
+}
